61dc8b765c6bc0a2db31a9570039ce3e23a3723b
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-09 08:35:11 -0500

    Bootstrap included and other modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+import { stateType } from './state/store'
+import Providers from './components/Providers'
+import Products from './components/Products'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {user} = useSelector((state: stateType) => state.logged)
 
   return (
     <div className="App">
       
+      <BrowserRouter>
+      <Navbar logged = {true}/>
+      <Routes>
+        <Route path='providers' element={<Providers/>}/>
+        <Route path='products' element={<Products/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
