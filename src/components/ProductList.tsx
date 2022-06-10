49c71c5046aab3f51e0a73b76d2fd3176e14c9dd
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-10 08:21:33 -0500

    Product and ProductList components

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IProductListProps {
+}
+
+const ProductList: React.FunctionComponent<IProductListProps> = (props) => {
+  return (
+      <h2>
+      Products go here!
+      </h2>
+  );
+};
+
+export default ProductList;
